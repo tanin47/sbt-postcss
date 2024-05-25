ea79ed35a5f4848f2718fe094b4e119bc9e2a538
Tanin Na Nakorn <@tanin>
2024-05-24 22:08:19 -0700

    Working code

---
+++ build.sbt
@@ -0,0 +1,19 @@
+name := "sbt-postcss"
+
+lazy val `sbt-postcss` = (project in file("."))
+  .enablePlugins(SbtWebBase)
+  .settings(
+    scalaVersion := "2.12.18",
+    libraryDependencies ++= Seq(
+      "com.typesafe.play" %% "play-json"   % "2.8.1",
+      "org.mockito"       % "mockito-core" % "3.0.0" % Test,
+      "com.lihaoyi"       %% "utest"       % "0.7.1" % Test
+    ),
+    testFrameworks += new TestFramework("utest.runner.Framework")
+  )
+
+enablePlugins(SbtWebBase)
+
+description := "sbt-web plugin for executing postcss-cli after the sbt-web source file tasks finish."
+
+addSbtJsEngine("1.2.3")
---
+++ src/main/scala/tanin/play/postcss/SbtPostcss.scala
@@ -0,0 +1,132 @@
+package tanin.play.postcss
+
+import sbt._
+import com.typesafe.sbt.web.{PathMapping, SbtWeb}
+import com.typesafe.sbt.web.js.JS
+import com.typesafe.sbt.web.pipeline.Pipeline
+import sbt.Keys._
+import org.apache.ivy.util.ChecksumHelper
+import sbt.Task
+import sbt.internal.util.ManagedLogger
+
+class Shell {
+  def execute(logger: ManagedLogger, cmd: String, cwd: File, envs: (String, String)*): Int = {
+    import scala.sys.process._
+
+    val envString = if (envs.nonEmpty) {
+      envs.toList.map { case (k, v) => s"$k=$v"}.mkString(" ") + " "
+    } else {
+      ""
+    }
+
+    logger.info(s"Current working dir: ${cwd.getCanonicalPath}")
+    logger.info(s"Executing: ${envString}${cmd}")
+
+    val exitCode = Process(cmd, cwd, envs: _*).!
+    logger.info(s"Exited with $exitCode")
+
+    exitCode
+  }
+
+  def fileExists(file: File): Boolean = file.exists()
+}
+
+object Import {
+
+  val postcss = TaskKey[Pipeline.Stage]("postcss", "Run postcss-cli")
+
+  object PostcssKeys {
+    val binaryFile = TaskKey[String]("postcss-cli-binary", "The postcss CLI binary.")
+    val inputFile = TaskKey[String]("postcss-input-file", "The input file for postcss-cli. We support only 1 input file for now.")
+    val nodeModulesPath = TaskKey[String]("postcss-node-modules-path", "The location of the node_modules. Default: ./node_modules")
+  }
+
+}
+
+object SbtPostcss extends AutoPlugin {
+
+  override def requires = SbtWeb
+
+  override def trigger = AllRequirements
+
+  val autoImport = Import
+
+  import SbtWeb.autoImport._
+  import WebKeys._
+  import autoImport._
+  import PostcssKeys._
+
+  override def projectSettings: Seq[Setting[_]] = Seq(
+    (postcss / includeFilter) := AllPassFilter,
+    (postcss / excludeFilter) := HiddenFileFilter,
+    (postcss / nodeModulesPath) := "./node_modules",
+    postcss := postcssStage.value
+  )
+
+  def postcssStage: Def.Initialize[Task[Pipeline.Stage]] = Def.task {
+    val include = (postcss / includeFilter).value
+    val exclude = (postcss / excludeFilter).value
+    val binaryFilePath = (postcss / binaryFile).value
+    val inputFilePath = (postcss / inputFile).value
+    val target = webTarget.value / postcss.key.label
+    val logger = (Assets / streams).value.log
+    val nodeModulesLocation = (postcss / nodeModulesPath).value
+    val shell = new Shell
+
+    { mappings => 
+      PostcssStage.run(
+        mappings = mappings.filter { case (file, output) => include.accept(file) && !exclude.accept(file) },
+        binaryFilePath = binaryFilePath,
+        inputFilePath = inputFilePath,
+        targetDir = target,
+        nodeModulesPath = nodeModulesLocation,
+        logger = logger,
+        shell = shell
+      )
+    }
+  }
+
+  object PostcssStage {
+
+    def run(
+      mappings: Seq[PathMapping],
+      binaryFilePath: String,
+      inputFilePath: String,
+      targetDir: File,
+      nodeModulesPath: String,
+      logger: ManagedLogger,
+      shell: Shell
+    ): Seq[PathMapping] = {
+      val inputFile = new File(inputFilePath)
+      val (inputEntries, ignoreds) = mappings
+        .partition { case (file, path) =>
+          file.getCanonicalPath == inputFile.getCanonicalPath
+        }
+
+      val outputFile = targetDir / inputFile.getName
+
+      val processeds = inputEntries
+        .map { case (file, path) =>
+          val cmd = Seq(
+            binaryFilePath,
+            inputFilePath,
+            "--config",
+            ".",
+            "--output",
+            outputFile.getCanonicalPath
+          ).mkString(" ")
+
+          shell.execute(logger, cmd, new File("."), "NODE_PATH" -> nodeModulesPath)
+
+          if (!shell.fileExists(outputFile)) {
+            throw new Exception(s"${outputFile.getCanonicalPath} should have been generated, but it wasn't.")
+          }
+
+          logger.info(s"Generated: ${outputFile.getCanonicalPath}")
+          outputFile -> path
+        }
+
+      ignoreds ++ processeds
+    }
+  }
+}
